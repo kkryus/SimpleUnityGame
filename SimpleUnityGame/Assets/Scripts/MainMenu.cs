e3accab597030233ce9f7f278fe4d112a5936afc
kkryus <kamilkryus@gmail.com>
2018-12-18 18:35:17 +0100

    JUST main menu

---
+++ SimpleUnityGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    public bool isStart;
+    public bool isQuit;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Mouse();
+        }
+	}
+
+    private void Mouse()
+    {
+        
+        if (isQuit)
+        {
+            Debug.Log("okooo");
+            Application.Quit();
+        }
+        if (isStart)
+        {
+            Debug.Log("2222");
+            SceneManager.LoadScene("SampleScene");
+        }  
+    }
+}
